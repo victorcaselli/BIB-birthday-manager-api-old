9a14ae04387be88c43d6be3af0996708b214bc08
Victor Caselli <vd.caselli@gmail.com>
2022-10-18 22:11:14 -0300

    project created

---
+++ src/main/java/br/com/casellisoftware/bibbirthdaymanagerapi/BibBirthdayManagerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.casellisoftware.bibbirthdaymanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibBirthdayManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibBirthdayManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/casellisoftware/bibbirthdaymanagerapi/BibBirthdayManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.casellisoftware.bibbirthdaymanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibBirthdayManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
